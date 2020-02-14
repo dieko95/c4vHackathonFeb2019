3be20466504b86c06e703b06bd2e552a091f70a4
Neel <nroshania@gmail.com>
2020-02-13 22:54:07 -0800

    MPLS Angostura R ETL

--- encuestaHospitales/Angostura_Quick_Start.R
+++ encuestaHospitales/Angostura_Quick_Start.R
@@ -4,21 +4,22 @@
 # https://cloud.google.com/ai-platform/notebooks/docs/use-r-bigquery
 
 
-
+# Library dependencies
 if (!require("bigrquery")) install.packages("bigrquery")
 if (!require("gargle")) install.packages("gargle")
 
+# get client json
+bq_auth(
+  bq_auth(
+    path = 'https://storage.googleapis.com/angostura-public/hult-hackathon-key.json'
+    )
+  )
 
-
-bq_auth(bq_auth(
-  path = 'https://storage.googleapis.com/angostura-public/hult-hackathon-key.json'
-))
-
-# Set your query
-sql <- 'SELECT * from `angostura_dev.eh_health_survey_response`'
-
-# Run the query and store the data in a dataframe
-df <- query_exec(sql, 'angostura_dev', use_legacy_sql = FALSE)
-
-# Print the query result
-df
+# # Set your query
+# sql <- 'SELECT * from `angostura_dev.eh_health_survey_response`'
+# 
+# # Run the query and store the data in a dataframe
+# df <- query_exec(sql, 'angostura_dev', use_legacy_sql = FALSE)
+# 
+# # Print the query result
+# df
